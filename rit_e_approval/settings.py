7981d93896cca59fbb9ecfa5f5ba61e5df919090
Dhanesh2ad010 <120025653+Dhanesh2ad010@users.noreply.github.com>
2024-06-21 12:06:57 +0530

    Revert "add user name"
    This reverts commit afd7bb6e6f312d9c997e166720fef6e042874408.

--- rit_e_approval/settings.py
+++ rit_e_approval/settings.py
@@ -79,7 +79,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ganeshperumal256@gmail.com'  # Your Gmail email address
-EMAIL_HOST_PASSWORD = 'idokrftlbpbnuhsx'
+EMAIL_HOST_PASSWORD = 'Denhash#'  
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
